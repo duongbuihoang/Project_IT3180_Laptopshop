31f53014b9d44e0a51893acbdac995e585fd6ad5
Duong Bui Hoang <duongbui28052006@gmail.com>
2026-09-18 00:52:09 +0700

    update src 18/9

--- src/main/java/vn/project/laptopshop/controller/UserController.java
+++ src/main/java/vn/project/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.project.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("duongbui", test);
+        model.addAttribute("test", "from controller with model");
         return "hello";
     }
 }
