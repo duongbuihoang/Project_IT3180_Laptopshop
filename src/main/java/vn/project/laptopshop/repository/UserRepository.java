46850cf1e55937b689bc43d0aa057d7fb9005329
Duong Bui Hoang <duongbui28052006@gmail.com>
2026-09-23 23:07:04 +0700

    feat: khoi tao source code ban dau cho du an

--- src/main/java/vn/project/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/project/laptopshop/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package vn.project.laptopshop.repository;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import vn.project.laptopshop.domain.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByResetKey(String resetKey);
+
     User save(User duongbui);
 }
